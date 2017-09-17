43cd7deb441cf304ea79716c98c3dfb74cc6da4e
michaljarnot <michal.jarnot@strv.com>
2017-09-17 18:27:12 +0200

    Create simple register component service function
    - so that we would abstract the Navigation.registerComponent logic

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,9 +4,14 @@ import Left from './Left'
 import Right from './Right'
 
 
+const registerComponent =
+  redux =>
+    (name, component) =>
+      Navigation.registerComponent(name, () => component, redux.store, redux.Provider)
+
 export function registerScreens(redux) {
-  Navigation.registerComponent('example.Left', () => Left, redux.store, redux.Provider)
-  Navigation.registerComponent('example.Right', () => Right, redux.store, redux.Provider)
+  registerComponent(redux)('example.Left', Left)
+  registerComponent(redux)('example.Right', Right)
 }
 
 export function registerScreenVisibilityListener() {
