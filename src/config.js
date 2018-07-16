6bb76dce96a324c67239f1fcdbd804f99385aee0
michaljarnot <m.jarnot@yahoo.com>
2018-07-16 13:36:20 +0200

    Update config file
    - remove todo comment because we don't want to use styled-theme anymore
    - update link to material color generator

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-// TODO: Add https://github.com/erikras/styled-components-theme
-// There is a cool color generator here: https://material.io/color/?gclid=EAIaIQobChMI6d-p59W31wIVBxIbCh2r8gS6EAAYASADEgLyMPD_BwE#!/?view.left=0&view.right=0&primary.color=66BB6A&primary.text.color=000000
+// Color generator: https://material.io/tools/color/#!/
 export const THEME = {
   primary: '#66bb6a',
   primaryLight: '#98ee99',
