bf0749ecef0a38c7a59a99103f6d3f4e8e0ae051
michaljarnot <m.jarnot@yahoo.com>
2018-09-17 00:26:31 +0200

    Implement react-native-navigation for ios

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
+import { Navigation } from 'react-native-navigation';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.playground.WelcomeScreen',
+      },
+    },
+  });
+});
