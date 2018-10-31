5c73cd012f1d36dc1903d97272c39b52f08d2eb5
michaljarnot <m.jarnot@yahoo.com>
2018-10-30 22:38:36 -0700

    Update jest config
    - add coverage reporters

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'react-native',
   verbose: true,
+  coverageReporters: ['json', 'lcov', 'text'],
   transformIgnorePatterns: [
     'node_modules/(?!react-native|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|tcomb-form-native)',
   ],
