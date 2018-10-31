1970092f8c0705e8339f8ca59c231e4a11737497
michaljarnot <m.jarnot@yahoo.com>
2018-10-30 22:40:57 -0700

    Do minor CI updates

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'react-native',
   verbose: true,
-  coverageReporters: ['json', 'lcov', 'text'],
   transformIgnorePatterns: [
     'node_modules/(?!react-native|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|tcomb-form-native)',
   ],
