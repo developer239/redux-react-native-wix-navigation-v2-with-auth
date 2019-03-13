32fce8bde5608770e37c73dbe6de53ebc6733340
Michal Jarnot <m.jarnot@yahoo.com>
2019-03-13 16:40:50 +0100

    Update dependencies 🌴

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,19 +3,19 @@ import is from 'styled-is'
 
 export default styled.View`
   flex: 1;
-  
+
   ${is('center')`
     align-items: center;
     justify-content: center;
   `};
-  
+
   ${is('marginHorizontal')`
-    margin-left: ${props => props.marginHorizontal}px;
-    margin-right: ${props => props.marginHorizontal}px;
+    margin-left: ${props => `${props.marginHorizontal}`}px;
+    margin-right: ${props => `${props.marginHorizontal}`}px;
   `};
-  
+
   ${is('marginVertical')`
-    margin-top: ${props => props.marginVertical}px;
-    margin-bottom: ${props => props.marginVertical}px;
+    margin-top: ${props => `${props.marginVertical}`}px;
+    margin-bottom: ${props => `${props.marginVertical}`}px;
   `};
 `
