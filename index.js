06c720d215c89642703854ec11ebf1cefba5da47
Michal Jarnot <m.jarnot@yahoo.com>
2019-03-14 18:18:00 +0100

    Implement RNN2 in android

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
+import { Navigation } from 'react-native-navigation'
+import { AppRegistry } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () => App)
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App)
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.playground.WelcomeScreen'
+      }
+    }
+  })
+})
