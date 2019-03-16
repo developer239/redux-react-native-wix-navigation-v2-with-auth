09471d8cc7e3fd1d64ef1ccc52d982f7e8ddf949
Michal Jarnot <m.jarnot@yahoo.com>
2019-03-16 22:46:23 +0100

    Add pre-commit hook

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,13 @@ import React, { FunctionComponent } from 'react'
 import { NativeBase, Text } from 'native-base'
 import { StyledButton } from './styled'
 
-const ButtonComponent: FunctionComponent<NativeBase.Button> = ({ children, ...props }) =>
-  <StyledButton {...props}><Text>{children}</Text></StyledButton>
+const ButtonComponent: FunctionComponent<NativeBase.Button> = ({
+  children,
+  ...props
+}) => (
+  <StyledButton {...props}>
+    <Text>{children}</Text>
+  </StyledButton>
+)
 
 export default ButtonComponent
